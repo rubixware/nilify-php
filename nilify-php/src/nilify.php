c583bae47c8d99f6ca2257c177cae1df55c63242
everblut <emedina@rubixware.com>
2015-09-28 20:42:22 -0500

    First commit

---
+++ nilify-php/src/nilify.php
@@ -0,0 +1,23 @@
+<?php
+
+trait Nilify {
+
+  public function nilify($methods){
+    self::nilify_methods($methods);
+  }
+
+  public function nilify_from($class_name){
+    nilify_methods($clas_name.instance_methods);
+  }
+
+  public function nilify_methods($methods){
+    foreach ($methods as $method) {
+      $this->{$method} = self::mock($method);
+    }
+  }
+
+  public function mock($method){
+    return $method . ' is a mock method';
+  }
+
+}
